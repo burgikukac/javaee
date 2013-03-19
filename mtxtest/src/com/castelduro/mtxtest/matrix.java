b83808edf87df39b4f43b1a32734f351f4c0adb9
Burgikukac <tamas.burghard@gmail.com>
2013-03-19 16:39:33 +0100

    elso nyomat
    Change-Id: I9e7e59e9719859e960c802cb5efd50a28efa28d2

---
+++ mtxtest/src/com/castelduro/mtxtest/matrix.java
@@ -0,0 +1,13 @@
+package com.castelduro.mtxtest;
+
+public class matrix {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HELLOBELLO");
+	}
+
+}
